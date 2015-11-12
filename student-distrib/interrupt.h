a6ce1bd58a36e8955db7d092edaa9f1e1dfdeddd
sxiong7768 <sxiong7768@gmail.com>
2015-11-12 14:28:41 -0600

    added my user stack function in interrupt.S and interrupt.h

---
+++ student-distrib/interrupt.h
@@ -0,0 +1,27 @@
+#ifndef _INTERRUPT_H
+#define _INTERRUPT_H
+
+#include "types.h"
+
+/* The actual handler for the keyboard is defined externally */
+extern void keyboard_int_handler();
+
+/* The actual handler for the rtc is defined externally */
+extern void rtc_int_handler();
+
+/* The handlers for all of the system calls */
+/*extern int32_t halt(uint8_t status);
+extern int32_t execute(const uint8_t* command);
+extern int32_t read(int32_t fd, void* buf, int32_t nbytes);
+extern int32_t write(int32_t fd, const void* buf, int32_t nbytes);
+extern int32_t open(const uint8_t* buf, int32_t nbytes);
+extern int32_t close(int32_t fd);
+extern int32_t getargs(uint8_t* buf, int32_t nbytes);
+extern int32_t vidmap(uint8_t** screen_start);
+extern int32_t set_handler(int32_t signum, void* handler_address);
+extern int32_t sigreturn(void);*/
+
+/* Function to reset the stack for User */
+extern void return_to_user(int32_t new_eip);
+
+#endif /* _INTERRUPT_H*/
