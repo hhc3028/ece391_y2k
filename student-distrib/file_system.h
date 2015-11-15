e66d3ebcb22c01e6f9258ad407f52c7f8f64a786
sxiong7768 <sxiong7768@gmail.com>
2015-11-15 17:05:14 -0600

    Update file_system.h

--- student-distrib/file_system.h
+++ student-distrib/file_system.h
@@ -26,6 +26,13 @@
 /* Initializing the File System in memory */
 void init_file_systems(uint32_t address);
 
+/* Checkpoint 3 Functions */
+int32_t filesystem_load(const uint8_t* fname, uint32_t address);
+int32_t filesystem_open(uint32_t address);
+int32_t filesystem_close(void);
+int32_t filesystem_read(const uint8_t* fname, uint32_t offset, uint8_t* buf, uint32_t length);
+int32_t filesystem_write(void);
+
 /* File System Utilies */
 int32_t read_dentry_by_name(const uint8_t* fname, dentries_t* dentry);
 int32_t read_dentry_by_index(uint32_t index, dentries_t* dentry);
